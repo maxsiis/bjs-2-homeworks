7f3d52a892b590f5970e41c06d3bfbb98251919a
maxsiis <maksim.aleksandrov.2016@bk.ru>
2025-02-17 18:14:54 +0300

    ошибка исправлена

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -29,26 +29,21 @@ console.log(solveEquation(1, 2, 1));
 console.log(solveEquation(2, 2, 2));
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let creditBody = amount - contribution;
-  if (creditBody <= 0) {
-    return 0;
+  if (!isNaN(percent) && !isNaN(contribution) && !isNaN(amount)) {
+    const perMonth = percent / 100 / 12;
+    const bodyCredit = amount - contribution;
+    const monthly =
+      bodyCredit * (perMonth + perMonth / ((1 + perMonth) ** countMonths - 1));
+    const totalSum = parseFloat((monthly * countMonths).toFixed(2));
+    return totalSum;
   }
-
-  let monthlyRate = percent / 100 / 12;
-
-  let payment =
-    creditBody *
-    (monthlyRate + monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1));
-
-  let totalPayment = payment * countMonths + contribution;
-
-  return parseFloat(totalPayment.toFixed(2));
+  return false;
 }
 
-console.log(calculateTotalMortgage(10, 0, 50000, 12));
 console.log(calculateTotalMortgage(10, 1000, 50000, 12));
-console.log(calculateTotalMortgage(10, 0, 20000, 24));
 console.log(calculateTotalMortgage(10, 1000, 20000, 24));
+console.log(calculateTotalMortgage(10, 0, 50000, 12));
+console.log(calculateTotalMortgage(10, 0, 20000, 24));
 console.log(calculateTotalMortgage(10, 20000, 20000, 24));
 console.log(calculateTotalMortgage(10, 0, 10000, 36));
 console.log(calculateTotalMortgage(15, 0, 10000, 36));
